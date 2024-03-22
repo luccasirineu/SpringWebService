a38729b0e222944b15c8fb33bc7c4869c22ba315
luccasirineu <luccas19100@outlook.com>
2024-03-22 18:37:58 -0300

    Fazendo o insert de User

--- src/main/java/com/luccasdev/project/resource/UserResource.java
+++ src/main/java/com/luccasdev/project/resource/UserResource.java
@@ -1,13 +1,17 @@
 package com.luccasdev.project.resource;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luccasdev.project.entities.User;
 import com.luccasdev.project.services.UserService;
@@ -19,7 +23,7 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 	
-@GetMapping // serve para dizer que este metodo é um GET
+@GetMapping // serve para dizer que este metodo é um GET e retorna algo do banco de dados
 	public ResponseEntity<List<User>> findAll(){ // ResponseEntity que retorna a requisição web
 	List<User> list = service.findAll();
 	return ResponseEntity.ok().body(list);
@@ -30,5 +34,12 @@ public ResponseEntity<User>findById(@PathVariable Long id){
 	User obj = service.findById(id);
 	return ResponseEntity.ok().body(obj);
 
+}
+
+@PostMapping // para postar algo no banco de dados
+public ResponseEntity<User> insert(@RequestBody User obj){ // RequestBody serve para o json fzr a deserialização do User
+	obj = service.insert(obj);
+	URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); // convertendo o obj em uri para fzr os post
+	return ResponseEntity.created(uri).body(obj);
 }
 }
