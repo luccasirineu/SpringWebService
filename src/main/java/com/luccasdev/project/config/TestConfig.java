af5d04019043812851856b0f89d43f9a1a5c53a3
luccasirineu <luccas19100@outlook.com>
2024-03-22 17:41:52 -0300

    Criando entidade payment e fazendo associacao OneToOne

--- src/main/java/com/luccasdev/project/config/TestConfig.java
+++ src/main/java/com/luccasdev/project/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.luccasdev.project.entities.Category;
 import com.luccasdev.project.entities.Order;
 import com.luccasdev.project.entities.OrderItem;
+import com.luccasdev.project.entities.Payment;
 import com.luccasdev.project.entities.Product;
 import com.luccasdev.project.entities.User;
 import com.luccasdev.project.enums.OrderStatus;
@@ -79,8 +80,10 @@ public class TestConfig implements CommandLineRunner { // implementa uma interfa
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
 
-
-
+		Payment payment1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(payment1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
--- src/main/java/com/luccasdev/project/entities/Order.java
+++ src/main/java/com/luccasdev/project/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.luccasdev.project.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -41,6 +43,9 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order") // pois é no id da classe OrderItem que esta o pedido(order)
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy="order", cascade = CascadeType.ALL) // MAPEANDO AS DUAS ENTIDADES PARA TEREM O MSM ID
+	private Payment payment;
+	
 	public Order() {}
 	
 	
@@ -51,6 +56,18 @@ public class Order implements Serializable {
 		this.moment = moment;
 		setOrderStatus(orderStatus);
 		this.client = client;
+	}
+
+
+
+	public Payment getPayment() {
+		return payment;
+	}
+
+
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 
